2c830f8f679daa0952aaa3eba988e4786cacdf25
Ankeet Maini <ankeet.maini@gmail.com>
2020-06-24 13:09:56 +0530

    Fix path for actions file (#25)
    * Fix path for actions file
    
    * Fix spaces
    
    * Removes a leading dash
    
    * fix type errors

--- src/apps/Shipper/Root.tsx
+++ src/apps/Shipper/Root.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import { Component } from "react";
 import MapComp from "../components/MapComp";
-import { Platform, StyleSheet, Text, View } from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload. Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload. Shake or press menu button for dev menu"
-});
+import { StyleSheet, View } from "react-native";
 
 interface Props {
   test: string;
