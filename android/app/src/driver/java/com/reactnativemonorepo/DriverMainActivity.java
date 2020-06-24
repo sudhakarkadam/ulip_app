8f8844d7ee72a5647eb7e39ad9a4a08c16eedc75
Ankeet Maini <ankeet.maini@gmail.com>
2020-06-24 17:59:18 +0530

    Bootstraps driver app (#31)
    Fixes #30

---
+++ android/app/src/driver/java/com/reactnativemonorepo/DriverMainActivity.java
@@ -0,0 +1,18 @@
+
+package com.reactnativemonorepo;
+
+import android.os.Bundle;
+
+import com.facebook.react.ReactActivity;
+
+public class DriverMainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Driver";
+    }
+}
