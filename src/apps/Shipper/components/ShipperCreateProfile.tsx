5c60fdbd5f6916364938f689882e895edcbe0cad
Mathan Appadurai <mathanpec@gmail.com>
2020-06-26 17:41:23 +0530

    Adds type

--- src/apps/Shipper/components/ShipperCreateProfile.tsx
+++ src/apps/Shipper/components/ShipperCreateProfile.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { StackScreenProps } from "@react-navigation/stack";
 import CardComp from "../../../components/CardComp";
 import { Flex } from "../../../components/@styled/BaseElements";
+import { RootStackParamList } from "./AuthenticatedFlow";
 
 const personIcon = require("../../../icons/person-icon.png");
 
-type ScreenProps = StackScreenProps<{ [key: string]: undefined }, "Profile">;
+type ScreenProps = StackScreenProps<RootStackParamList, "CreateProfile">;
 
 const ShipperCreateProfile = (props: ScreenProps) => {
   return (
