79e4dd0cb5ee83214a45e8d32fcfdbf199034b91
apurvjain95 <apurvjain95@gmail.com>
2020-06-26 14:05:00 +0530

    Icons added + Login corrected (#40)

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Flex, TouchableOpacity } from "./@styled/BaseElements";
+import { Flex, TouchableOpacity, FlexColumn } from "./@styled/BaseElements";
+import { Text, TextInput } from "react-native";
 import colors from "../theme/colors";
 import Logo from "../images/group.svg";
 import BackBtn from "../images/arrow-left-circle.svg";
@@ -23,26 +24,41 @@ const LoginComponent = () => {
       <Flex flex={1} justifyContent="space-evenly">
         <Logo width={300} height={66} />
         {!phoneConfirmed && (
-          <>
-            <Input
-              label="mobile number"
-              keyboardType="numeric"
-              maxLength={10}
-              value={phoneNumber}
-              onChangeText={text => editPhoneNumber(text)}
-              style={{ marginBottom: 10 }}
-            />
-            {phoneNumber.length === 10 && (
-              <Flex alignItems="flex-end">
-                <StyledButton
-                  title="confirm"
-                  onPress={() => {
-                    setPhoneConfirmed(true);
-                  }}
-                />
-              </Flex>
-            )}
-          </>
+          <Flex>
+            <PrimaryText style={{ textTransform: "uppercase" }}>
+              Mobile Number
+            </PrimaryText>
+            <Flex
+              style={{
+                height: 50,
+                width: 300,
+                borderRadius: 5,
+                paddingLeft: 12,
+                paddingRight: 15,
+                marginTop: 5,
+                borderColor: colors.grays[1],
+                borderWidth: 1,
+                flexDirection: "row",
+                alignItems: "center"
+              }}
+            >
+              <Text style={{ fontSize: 18, color: colors.primary }}>
+                +91 -{" "}
+              </Text>
+              <TextInput
+                keyboardType="numeric"
+                maxLength={10}
+                value={phoneNumber}
+                onChangeText={text => editPhoneNumber(text)}
+                style={{
+                  fontSize: 18,
+                  color: colors.primary,
+                  display: "flex",
+                  flex: 1
+                }}
+              />
+            </Flex>
+          </Flex>
         )}
         {phoneConfirmed && (
           <Flex alignItems="center">
@@ -81,6 +97,22 @@ const LoginComponent = () => {
                 OTP Successfully Verified for {phoneNumber}
               </PrimaryText>
             )}
+          </Flex>
+        )}
+      </Flex>
+      <Flex style={{ justifyContent: "flex-end" }}>
+        {phoneNumber.length === 10 && !phoneConfirmed && (
+          <Flex style={{ paddingBottom: 10 }}>
+            <StyledButton
+              title="Confirm"
+              onPress={() => setPhoneConfirmed(true)}
+              style={{
+                textAlign: "center",
+                color: `${colors.white}`,
+                textTransform: "uppercase",
+                fontSize: 16
+              }}
+            />
           </Flex>
         )}
       </Flex>
