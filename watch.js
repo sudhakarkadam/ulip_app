092f3264d1aff016f8bfc0ba08257ff642d14268
Ankeet Maini <ankeet.maini@gmail.com>
2020-07-23 23:55:35 +0530

    watches for types (#137)

---
+++ watch.js
@@ -0,0 +1,11 @@
+var watch = require("node-watch");
+const execFile = require("child_process").execFile;
+const spawn = require("child_process").spawn;
+const path = require("path");
+watch("./i18n/en.json", { recursive: false }, () => {
+  execFile("node", [path.resolve(__dirname, "translation-types.js")]);
+
+  spawn("./node_modules/.bin/prettier --write 'src/typings/translation.ts'", {
+    shell: true
+  });
+});
